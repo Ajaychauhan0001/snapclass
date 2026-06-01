5f50505ce30103549d09fbd56c8f09b91fdbd0bb
Ajaychauhan0001 <ajaychauhan14160@gmail.com>
2026-06-01 20:40:33 +0530

    Initial commit

---
+++ src/components/header.py
@@ -0,0 +1,12 @@
+import streamlit as st 
+
+def header_home():
+    st.image(r"C:\Users\pc\Downloads\ChatGPT Image May 30, 2026, 09_17_33 PM.png", width=500)
+        
+                
+                
+    
+    
+    
+    
+    
