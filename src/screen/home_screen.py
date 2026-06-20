c0911e2ee466f8cfbc794cf0d872deede209ac4d
Ajaychauhan0001 <ajaychauhan14160@gmail.com>
2026-06-20 18:07:37 +0530

    Use relative paths for home screen images

--- src/screen/home_screen.py
+++ src/screen/home_screen.py
@@ -3,26 +3,21 @@ from src.components.header import header_home
 from src.ui.base_layout import style_base_layout, style_background_home
 from src.components.footer import footer_home
 
-
-def Home_screen(): 
+def Home_screen():
     header_home()
     style_background_home()
     style_base_layout()
-    
     col1, col2 = st.columns(2)
-    
     with col1:
         st.header("I am Student")
-        st.image(r"C:\Users\pc\Downloads\Gemini_Generated_Image_wuacj4wuacj4wuac.png", width=120)
+        st.image("assets/student_icon.png", width=120)
         if st.button('Student Portal', type='primary', icon=':material/arrow_outward:', icon_position='right'):
-            st.session_state['login_type'] = 'Student'   # ← lowercase
-            st.rerun() 
-        
+            st.session_state['login_type'] = 'Student'
+            st.rerun()
     with col2:
         st.header("I am Teacher")
-        st.image(r"C:\Users\pc\Downloads\Gemini_Generated_Image_sdc7hxsdc7hxsdc7.png", width=130)
+        st.image("assets/teacher_icon.png", width=130)
         if st.button('Teacher Portal', type='primary', icon=':material/arrow_outward:', icon_position='right'):
-            st.session_state['login_type'] = 'Teacher'   # ← lowercase
+            st.session_state['login_type'] = 'Teacher'
             st.rerun()
-            
     footer_home()
